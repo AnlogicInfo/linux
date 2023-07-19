ee997114f0ffb6aba700d6fbebe9bb06c0178f5f
naiyuan.que <naiyuan.que@anlogic.com>
2023-07-19 11:31:39 +0800

    change project's "al9000" & "nuclei" to "dr1v90" & "dr1m90"

--- make_rpu.sh
+++ make_rpu.sh
@@ -1,13 +1,13 @@
 #defconfig
-#make ARCH=riscv CROSS_COMPILE=../toolchain/riscv/gcc/bin/riscv-nuclei-linux-gnu- al9000_rpu_defconfig
-cp arch/riscv/configs/anlogic_al9000_rpu_defconfig .config
-make ARCH=riscv CROSS_COMPILE=../toolchain/riscv/gcc/bin/riscv-nuclei-linux-gnu- olddefconfig
+make ARCH=riscv CROSS_COMPILE=../toolchains/riscv/gcc/bin/riscv-nuclei-linux-gnu- anlogic_dr1v90_defconfig
+# cp arch/riscv/configs/anlogic_dr1v90_defconfig .config
+make ARCH=riscv CROSS_COMPILE=../toolchains/riscv/gcc/bin/riscv-nuclei-linux-gnu- olddefconfig
 
 #make dtbs
-make ARCH=riscv CROSS_COMPILE=../toolchain/riscv/gcc/bin/riscv-nuclei-linux-gnu- dtbs
+make ARCH=riscv CROSS_COMPILE=../toolchains/riscv/gcc/bin/riscv-nuclei-linux-gnu- dtbs
 
 #Image
-make ARCH=riscv CROSS_COMPILE=../toolchain/riscv/gcc/bin/riscv-nuclei-linux-gnu- Image -j32
+make ARCH=riscv CROSS_COMPILE=../toolchains/riscv/gcc/bin/riscv-nuclei-linux-gnu- Image -j8
 
 #gzip
 mkimage -A riscv -O linux -T kernel -C gzip -a 0x00400000 -e 0x00400000 -n Linux -d arch/riscv/boot/Image.gz arch/riscv/boot/uImage.gz
