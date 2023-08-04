1c9f65f9155990451b1cbacd0e4d084a22f5bd08
naiyuan.que <naiyuan.que@anlogic.com>
2023-08-04 15:06:28 +0800

    Merge multiple configs into one. (nand,nor,mmc,gbe)

--- make_apu.sh
+++ make_apu.sh
@@ -1,11 +1,11 @@
 #defconfig
-make ARCH=arm64 CROSS_COMPILE=/opt/toolchain/7.5.0/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- anlogic_dr1m90_mmc_defconfig
+make ARCH=arm64 CROSS_COMPILE=/opt/toolchain/7.5.0/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- anlogic_dr1m90_defconfig
 
 #dtbs
 make ARCH=arm64 CROSS_COMPILE=/opt/toolchain/7.5.0/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- dtbs
 
 #Image
-make ARCH=arm64 CROSS_COMPILE=/opt/toolchain/7.5.0/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- -j32
+make ARCH=arm64 CROSS_COMPILE=/opt/toolchain/7.5.0/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- -j8
 
 #gzip
 mkimage -A arm64 -O linux -T kernel -C gzip -a 0x00400000 -e 0x00400000 -n Linux -d arch/arm64/boot/Image.gz arch/arm64/boot/uImage.gz
